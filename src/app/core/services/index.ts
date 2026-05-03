48944e5a335069a35a3f526cc1e3df90ac0b2116
Lucas Gauna <lucasdeveloper98@gmail.com>
2026-05-03 18:58:38 -0300

    refactor: add barrel exports

---
+++ src/app/core/services/index.ts
@@ -0,0 +1,5 @@
+export * from './hero.service';
+export * from './loading.service';
+export * from './navigation.service';
+export * from './notification.service';
+export * from './seo.service';
---
+++ src/app/shared/components/index.ts
@@ -0,0 +1,3 @@
+export * from './confirm-dialog/confirm-dialog.component';
+export * from './paginator/paginator.component';
+export * from './sidenav/sidenav.component';
